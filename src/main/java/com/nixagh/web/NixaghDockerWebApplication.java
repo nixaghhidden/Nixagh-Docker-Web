2b6dcd7fcbed16b4936434b373071470c06e4709
nghia.nguyen-dinh <nghia.nguyen-dinh@banvien.com.vn>
2024-03-28 13:21:50 +0700

    init

---
+++ src/main/java/com/nixagh/web/NixaghDockerWebApplication.java
@@ -0,0 +1,13 @@
+package com.nixagh.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixaghDockerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NixaghDockerWebApplication.class, args);
+    }
+
+}
